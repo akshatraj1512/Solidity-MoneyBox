5f0bbe26603db6a53a41507d088857922839f8e2
akshatraj1512 <ar221jm@gmail.com>
2025-02-10 14:45:21 +0530

    improved security & added withdrawal function

--- .deps/contracts/Moneybox.sol
+++ .deps/contracts/Moneybox.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.0;
 
 contract Moneybox {
     uint256 public bal;
+    address private owner;
 
     receive() external payable { //0x5B3731441Fda89A4446116A4891313023A3B5A2B
         bal += msg.value;
+    }
+
+    function withdraw(uint amt, address payable destAdd) public {
+        require(msg.sender == owner, "only owner can withdraw");
+        require(amt <= bal, "not enough money in your wallet");
+
+        destAdd.transfer(amt);
+        bal -= amt;
     }
 }
